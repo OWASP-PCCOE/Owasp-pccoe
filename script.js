78c202a0cfdb4f1f7455ba68917cc3615578169a
SumitPisal <sumit.pisal22@pccoepune.org>
2024-08-31 14:13:53 +0530

    fix icons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-hamburger=document.querySelector(".hamburger");
-hamburger.onclick = function() {
+const hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function () {
     navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
 }
